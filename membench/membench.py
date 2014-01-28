74541999ae137bdd4f18d5944ef4611cc238d5aa
Nicolas Savva <nss45@cornell.edu>
2014-01-28 08:35:19 -0500

    Made minor changes to membench for HW0

--- membench/membench.py
+++ membench/membench.py
@@ -1,5 +1,6 @@
 #!/share/cs-instructional/cs5220/local/anaconda/bin/python
 
+import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,11 +14,11 @@ def size_name(s):
         return "{0}K".format(ksize)
 
 # Load data from timings.csv and plot it, grouping by size
-df = pd.read_csv("timings.csv")
+df = pd.read_csv(sys.argv[1])
 for key, grp in df.groupby("size"):
     plt.semilogx(grp['stride'], grp['ns'], label=size_name(key), basex=2)
 lgd = plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 plt.xlabel('Stride (bytes)')
 plt.ylabel('Time (ns)')
-plt.savefig('membench.pdf', bbox_extra_artists=(lgd,), bbox_inches='tight')
+plt.savefig(sys.argv[1]+'.pdf', bbox_extra_artists=(lgd,), bbox_inches='tight')
 
